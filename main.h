8e427f950afed6f139e188012a21c97e90173d58
Mukheee <101358202+Mukheee@users.noreply.github.com>
2022-05-18 19:29:18 +0100

    update

--- main.h
+++ main.h
@@ -1,52 +1,36 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* libraries to use */
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
+#include <stddef.h>
+#include <string.h>
 
 /**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
+ * struct pr - struct for operations
+ * @op: operation
+ * @f: to be listed
+ *
+ * Description: Checks if there's an operation
  */
-typedef struct printHandler
+typedef struct pr
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-char *convert(unsigned long int num, int base, int lowercase);
-int _printf(const char *format, ...);
-int (*get_print(char s))(va_list, flags_t *);
-int get_flag(char s, flags_t *f);
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
+	char *op;
+	int (*f)(va_list);
+} pr_f;
+
+/* Prototypes */
 int _putchar(char c);
-int _puts(char *str);
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-int print_address(va_list l, flags_t *f);
-int print_percent(va_list l, flags_t *f);
+int _printf(const char *format, ...);
+int print_c(va_list list);
+int print_s(va_list list);
+int print_i(va_list list);
+int print_d(va_list list);
+int print_r(va_list list);
+int print_mod(va_list list);
+int print_d(va_list list);
+int _strlen(char *s);
+int _funcion(const char *format, pr_f ops[], va_list ap);
 #endif
