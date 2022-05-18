d5bfd098aed1c0ab9ab70c033c017b9575bdd6fe
Mukheee <101358202+Mukheee@users.noreply.github.com>
2022-05-18 18:50:05 +0100

    update main.h

--- main.h
+++ main.h
@@ -10,20 +10,23 @@
  *
  * Description: Checks if there's an operation
  */
-typedef struct pr
+typedef struct printf
 {
-	char *op;
+	char *p;
 	int (*f)(va_list);
 } pr_f;
 
 /* Prototypes */
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list list);
-int print_s(va_list list);
-int print_i(va_list list);
-int print_d(va_list list);
-int print_r(va_list list);
+int print_char(va_list list);
+int print_unsigned(va_list list);
+int print_nbr(va_list list);
+int print_octa(va_list list);
+int print_hex(va_list list);
+int print_string(va_list list);
+int print_percent(va_list list);
+int print_unassignednumber(va_list list);
 int print_mod(va_list list);
 int print_d(va_list list);
 int _strlen(char *s);
